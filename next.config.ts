8bc4a089e92d82c89d65131bfd940414ec49915a
xomcodes <xommyb@gmail.com>
2025-08-25 16:13:18 +0100

    vercel image ish

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["www.hvcproject.com", "hvcproject.com"],
+  },
 };
 
 export default nextConfig;
